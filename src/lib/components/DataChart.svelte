d1aad3895d2fac385d04e61093e3846d5babbf59
Lasse Wolpmann <33729366+lassewolpmann@users.noreply.github.com>
2024-04-09 10:58:54 +0300

    Moved Chart to seperate Component

---
+++ src/lib/components/DataChart.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+    import { Chart, type ChartConfiguration, type ChartOptions } from "chart.js/auto";
+    import { onMount } from "svelte";
+
+    export let dates: string[], tempData: number[], humidityData: number[];
+
+    let tempChart: HTMLCanvasElement;
+
+    const chartData = {
+        labels: dates,
+        datasets: [
+            {
+                label: "Temperature",
+                data: tempData,
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1,
+                yAxisID: 'y_temp'
+            },
+            {
+                label: "Humidity",
+                data: humidityData,
+                fill: false,
+                borderColor: 'rgb(255, 50, 50)',
+                tension: 0.1,
+                yAxisID: 'y_humidity'
+            }
+        ]
+    }
+
+    const chartOptions: ChartOptions = {
+        responsive: true,
+        scales: {
+            y_temp: {
+                type: 'linear',
+                display: true,
+                position: 'left',
+                title: {
+                    display: true,
+                    text: 'Temperature in °C'
+                },
+                ticks: {
+                    callback: function(value: string | number) {
+                        return value + ' °C';
+                    }
+                }
+            },
+            y_humidity: {
+                type: 'linear',
+                display: true,
+                position: 'right',
+                title: {
+                    display: true,
+                    text: 'Relative Humidity in %'
+                },
+                grid: {
+                    drawOnChartArea: false
+                },
+                ticks: {
+                    callback: function(value: string | number) {
+                        return value + ' %rH';
+                    }
+                }
+            }
+        }
+    }
+
+    const chartConfig: ChartConfiguration = {
+        type: 'line',
+        data: chartData,
+        options: chartOptions
+    }
+
+    onMount(() => {
+        const ctx = tempChart.getContext('2d');
+        if (ctx) {
+            new Chart(ctx, chartConfig);
+        }
+    })
+</script>
+<style>
+
+</style>
+<div>
+    <canvas bind:this={tempChart} id="temp-chart"></canvas>
+</div>
---
+++ src/lib/components/DateRangeSelection.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+
+</script>
+<style>
+
+</style>
+<div class="date-range-selection">
+    <!-- <button>Select Time Frame</button> -->
+</div>
